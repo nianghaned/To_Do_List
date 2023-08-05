339e2ba7a568def25113cb25ac5076b656dee0bc
nianghaned <31428064+nianghaned@users.noreply.github.com>
2023-08-05 00:02:07 -0400

    fixed typo in button id and in function

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-/* Whenever someone types and submit it should be added as an unorder
-list to the page.
-Listen to click for the button
-select the input and bring it into js
-save the input into a variable
-append what a user inputs into the list */
 const inputEl = document.getElementById("ToDoInput");
-const buttonEl = document.querySelectorAll("button");
+const buttonEl = document.getElementById("buttonInput");
 const listEl = document.getElementById("list");
 
-buttonEl.addEventListener("click", addToDo);
+buttonEl.addEventListener("click", addTask);
 
-function addToDo() {
+function addTask() {
   const inputText = inputEl.value;
   if (inputText === "") return;
   const inputItem = document.createElement("li");
-  toDoItem.innerText = inputText;
+  inputItem.innerText = inputText;
 
   listEl.appendChild(inputItem);
   inputEl.value = "";

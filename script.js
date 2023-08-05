a6aa81e2e9d2bea5a07e1c8a1b503df89c7a19f6
nianghaned <31428064+nianghaned@users.noreply.github.com>
2023-08-04 23:42:54 -0400

    added function logic for adding new to do to list

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ append what a user inputs into the list */
 const inputEl = document.getElementById("ToDoInput");
 const buttonEl = document.querySelectorAll("button");
 const listEl = document.getElementById("list");
+
+buttonEl.addEventListener("click", addToDo);
+
+function addToDo() {
+  const inputText = inputEl.value;
+  if (inputText === "") return;
+  const inputItem = document.createElement("li");
+  toDoItem.innerText = inputText;
+
+  listEl.appendChild(inputItem);
+  inputEl.value = "";
+}
